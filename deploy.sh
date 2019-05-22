609e9fd11cf2d02a19a276944d43fae22da1e268
Kamran Ayub <kamran.ayub@gmail.com>
2019-05-22 07:25:27 -0500

    Use separate properties

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 TAG=$1
 KEY=$2
 
-dotnet pack -c Release -o ../dist IGDB/IGDB.csproj -p:PackageVersion=$TAG;Title="IGDB API Wrapper";Authors="kayub";PackageDescription="A .NET Standard wrapper around the IGDB v3 games database API";PackageLicenseExpression="Apache-2.0";PackageTags="igdb;games;api;wrapper"
+dotnet pack -c Release -o ../dist IGDB/IGDB.csproj \
+  -p:PackageVersion=$TAG \
+  -p:Title="IGDB API Wrapper" \
+  -p:Authors="kayub" \
+  -p:PackageDescription="A .NET Standard wrapper around the IGDB v3 games database API" \
+  -p:PackageLicenseExpression="Apache-2.0" \
+  -p:PackageTags="igdb;games;api;wrapper"
 
 dotnet nuget push dist/IGDB.$TAG.nupkg -k $KEY -s https://www.nuget.org
