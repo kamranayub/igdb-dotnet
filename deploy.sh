f5a83a84e879a1c73b571039073874810b386beb
Kamran Ayub <kamran.ayub@gmail.com>
2019-05-22 00:55:58 -0500

    Fix nupkg name

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ KEY=$2
 
 dotnet pack -c Release -o ../dist IGDB/IGDB.csproj -p:PackageVersion=$TAG;Title="IGDB API Wrapper";Authors="kayub";PackageDescription="A .NET Standard wrapper around the IGDB v3 games database API";PackageLicenseExpression="Apache-2.0";PackageTags="igdb;games;api;wrapper"
 
-dotnet nuget push ./dist/IGDB-$TAG.nupkg -k $KEY -s https://www.nuget.org
+dotnet nuget push dist/IGDB.$TAG.nupkg -k $KEY -s https://www.nuget.org
