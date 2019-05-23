c3010f94efafda803620caf3f5a33d8c79f6e370
Kamran Ayub <kamran.ayub@gmail.com>
2019-05-22 23:17:29 -0500

    Better nuget packing using nuspec

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,6 @@
 TAG=$1
 KEY=$2
 
-dotnet pack -c Release -o ../dist IGDB/IGDB.csproj \
-  -p:PackageVersion=$TAG \
-  -p:Title="IGDB API Wrapper" \
-  -p:Authors="kayub" \
-  -p:PackageDescription="A .NET Standard wrapper around the IGDB v3 games database API" \
-  -p:PackageLicenseExpression="Apache-2.0" \
-  -p:PackageTags="igdb;games;api;wrapper"
+dotnet pack -c Release -o ../dist IGDB/IGDB.csproj -p:PackageVersion=$TAG -p:NuspecFile=../IGDB.nuspec
 
 dotnet nuget push dist/IGDB.$TAG.nupkg -k $KEY -s https://www.nuget.org
