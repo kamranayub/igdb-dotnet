4186c7582de9bfd40356ea77b00e0f3d3afb0315
Kamran Ayub <kamran.ayub@gmail.com>
2023-05-18 10:47:29 -0500

    feat: add more external games

--- IGDB/Models/ExternalGame.cs
+++ IGDB/Models/ExternalGame.cs
@@ -47,13 +47,20 @@ namespace IGDB.Models
     Twitch = 14,
     Android = 15,
     Playstation = 16,
-    XboxLiveTitle = 18,
     GamesPress = 19,
     AmazonAsin = 20,
     AmazonLuna = 22,
     AmazonAdg = 23,
     PlayAsia = 24,
     EpicGameStore = 26,
-    Oculus = 28
+    Oculus = 28,
+    Utomik = 29,
+    ItchIO = 30,
+    XboxMarketplace = 31,
+    Kartridge = 32,
+    PlaystationStoreUS = 36,
+    FocusEntertainment = 37,
+    XboxGamePassUltimateCloud = 54,
+    Gamejolt = 55
   }
 }
