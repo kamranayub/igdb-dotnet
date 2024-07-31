40e7591e5bc7cc94c821efea3493014589fd0ded
WaldoAndFriends <hello@waldoandfriends.tv>
2024-07-31 22:28:42 +0200

    Add PopScore endpoints
    Even though the IGDB API documentation states that the checksum field
    is available for the Popularity Primitive model, the API does not
    return it. This is why the Checksum property is commented out for now.

---
+++ IGDB/Models/PopularitySource.cs
@@ -0,0 +1,7 @@
+namespace IGDB.Models
+{
+	public enum PopularitySource
+	{
+		Igdb = 121
+	}
+}
---
+++ IGDB/Models/PopularityType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IGDB.Models
+{
+	public class PopularityType : ITimestamps, IHasChecksum
+	{
+		public string Checksum { get; set; }
+		public DateTimeOffset? CreatedAt { get; set; }
+		public string Name { get; set; }
+		public PopularitySource? PopularitySource { get; set; }
+		public DateTimeOffset? UpdatedAt { get; set; }
+	}
+}
