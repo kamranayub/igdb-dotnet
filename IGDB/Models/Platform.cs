c7385a15308cd6600afd0d3591e10efcc4f1db04
codename-irvin <brian.budge@protonmail.com>
2025-05-07 17:05:08 -0400

    Update Platform to adhere to IGDB changes

--- IGDB/Models/Platform.cs
+++ IGDB/Models/Platform.cs
@@ -6,7 +6,6 @@ namespace IGDB.Models
   {
     public string Abbreviation { get; set; }
     public string AlternativeName { get; set; }
-    public PlatformCategory? Category { get; set; }
     public string Checksum { get; set; }
     public DateTimeOffset? CreatedAt { get; set; }
     public int? Generation { get; set; }
@@ -14,21 +13,13 @@ namespace IGDB.Models
     public string Name { get; set; }
     public IdentityOrValue<PlatformFamily> PlatformFamily { get; set; }
     public IdentityOrValue<PlatformLogo> PlatformLogo { get; set; }
+    public IdentityOrValue<PlatformType> PlatformType { get; set; }
+    
     public string Slug { get; set; }
     public string Summary { get; set; }
     public DateTimeOffset? UpdatedAt { get; set; }
     public string Url { get; set; }
     public IdentitiesOrValues<PlatformVersion> Versions { get; set; }
     public IdentitiesOrValues<PlatformWebsite> Websites { get; set; }
-  }
-
-  public enum PlatformCategory
-  {
-    Console = 1,
-    Arcade = 2,
-    Platform = 3,
-    OperatingSystem = 4,
-    PortableConsole = 5,
-    Computer = 6
   }
 }
---
+++ IGDB/Models/PlatformType.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace IGDB.Models
+{
+  public class PlatformType : ITimestamps, IIdentifier, IHasChecksum
+  {
+    public string Checksum { get; set; }
+    public DateTimeOffset? CreatedAt { get; set; }
+    public string Name { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+    public long? Id { get; set; }
+  }
+}
